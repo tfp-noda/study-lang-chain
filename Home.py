bc062e8fcae71c38ce3db3aff0bb8dd1fec7ccd7
Kenta Noda <k.noda@tfp-group.co.jp>
2024-05-29 13:07:05 +0900

    first commit

---
+++ Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Home")
+
+st.subheader("Welcome to the Home Page")
+
+
+st.markdown(
+"""
+    hello   
+"""
+)
+
+
+st.write("This is a simple example of a Streamlit app.")
